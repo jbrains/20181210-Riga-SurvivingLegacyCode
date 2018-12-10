eb2c044e36c0d06e571d2f819b8cd0a0c9c14e75
J. B. Rainsberger <me@jbrains.ca>
2018-12-10 16:06:37 +0200

    We learned that we need to add a player in order to roll a die safely.

--- src/test/java/ca/jbrains/trivia/test/RollDieTest.java
+++ src/test/java/ca/jbrains/trivia/test/RollDieTest.java
@@ -6,7 +6,12 @@ import org.junit.Test;
 public class RollDieTest {
     @Test
     public void happyPath() throws Exception {
-        final Game game = new Game();
+        final Game game = new Game() {
+            {
+                add("::the only player's name::");
+            }
+        };
+        
         game.roll(3);
     }
 }
