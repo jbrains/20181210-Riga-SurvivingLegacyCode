50fa26a2046f62982dde0ffc5dc66f1fc96e0353
J. B. Rainsberger <me@jbrains.ca>
2018-12-10 16:08:48 +0200

    We can now check the new place of the only player of a game after rolling a die.

--- src/test/java/ca/jbrains/trivia/test/RollDieTest.java
+++ src/test/java/ca/jbrains/trivia/test/RollDieTest.java
@@ -1,17 +1,26 @@
 package ca.jbrains.trivia.test;
 
 import com.adaptionsoft.games.uglytrivia.Game;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class RollDieTest {
     @Test
     public void happyPath() throws Exception {
-        final Game game = new Game() {
-            {
-                add("::the only player's name::");
-            }
-        };
-        
+        final SinglePlayerGame game = new SinglePlayerGame();
+
         game.roll(3);
+
+        Assert.assertEquals(3, game.thePlaceOfTheOnlyPlayer());
+    }
+
+    private static class SinglePlayerGame extends Game {
+        {
+            add("::the only player's name::");
+        }
+
+        public int thePlaceOfTheOnlyPlayer() {
+            return places[0];
+        }
     }
 }
