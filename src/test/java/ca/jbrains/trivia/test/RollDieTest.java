6db390c26430e5123e031d4b95593c386e3a2410
J. B. Rainsberger <me@jbrains.ca>
2018-12-10 16:05:20 +0200

    We confirm that the Game constructor doesn't blow up on its own.

---
+++ src/test/java/ca/jbrains/trivia/test/RollDieTest.java
@@ -0,0 +1,11 @@
+package ca.jbrains.trivia.test;
+
+import com.adaptionsoft.games.uglytrivia.Game;
+import org.junit.Test;
+
+public class RollDieTest {
+    @Test
+    public void happyPath() throws Exception {
+        new Game();
+    }
+}
