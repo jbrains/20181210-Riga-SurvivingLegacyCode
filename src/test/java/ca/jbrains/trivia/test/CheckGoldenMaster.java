d55488e5101cdea47416334727d706afa9eefa94
J. B. Rainsberger <me@jbrains.ca>
2018-12-10 17:52:25 +0200

    Fixed a mistake in the Golden Master test, which was redirecting stdout without replacing it at the end of the test.

--- src/test/java/ca/jbrains/trivia/test/CheckGoldenMaster.java
+++ src/test/java/ca/jbrains/trivia/test/CheckGoldenMaster.java
@@ -1,14 +1,30 @@
 package ca.jbrains.trivia.test;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class CheckGoldenMaster {
+
+    private PrintStream productionStdout;
+
+    @Before
+    public void rememberStdout() throws Exception {
+        productionStdout = System.out;
+    }
+
+    @After
+    public void replaceStdout() throws Exception {
+        System.setOut(productionStdout);
+    }
+    
     @Test
     public void checkGoldenMaster() throws Exception {
         final ByteArrayOutputStream canvas = new ByteArrayOutputStream();
