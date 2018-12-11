3d0ce5ad4025a817a8ec7ceef256774becb1fea2
J. B. Rainsberger <me@jbrains.ca>
2018-12-11 11:18:54 +0200

    Documented behavior in Game that prevents simplifying the design.

--- src/test/java/ca/jbrains/trivia/test/RollDieWhenPlayerIsNotInPenaltyBoxTest.java
+++ src/test/java/ca/jbrains/trivia/test/RollDieWhenPlayerIsNotInPenaltyBoxTest.java
@@ -21,6 +21,20 @@ public class RollDieWhenPlayerIsNotInPenaltyBoxTest {
         Assert.assertEquals(5, movePlayer(11, 6));
     }
 
+    @Test
+    public void theBoardAppearsBrokenIfWeGoFarEnough() throws Exception {
+        // I infer from the code that the game board _should_ be circular,
+        // but currently the board does not behave as a perfect circle.
+        // In production, this "should never be a problem", but it's not clear
+        // to me whether we can safely "fix" the board to behave like a circle.
+
+        // If we could make the board into a circle, then that would simplify
+        // the code significantly!
+
+        Assert.assertEquals("Is the board circular now?", 23 + 1 - 12, movePlayer(23, 1));
+        Assert.assertEquals("Is the board circular now?", 11 + 28 - 12, movePlayer(11, 28));
+    }
+
     private int movePlayer(final int fromPlace, final int byRolling) {
         final SinglePlayerGame game = new SinglePlayerGameBuilder().withPlayerInPenaltyBox(false).withPlayerStartingAtPlace(fromPlace).build();
 
