dddaeadb2fd004578db1165aa141673adfe1785c
J. B. Rainsberger <me@jbrains.ca>
2018-12-11 11:08:19 +0200

    We can now build Single Player Game objects more flexibly for testing.

--- src/test/java/ca/jbrains/trivia/test/RollDieTest.java
+++ src/test/java/ca/jbrains/trivia/test/RollDieTest.java
@@ -7,26 +7,37 @@ import org.junit.Test;
 public class RollDieTest {
     @Test
     public void happyPath() throws Exception {
-        final SinglePlayerGame game = SinglePlayerGame.withPlayerNotInPenaltyBoxStartingAtPlace(0);
+        final SinglePlayerGame game = new SinglePlayerGameBuilder().withPlayerInPenaltyBox(false).withPlayerStartingAtPlace(0).build();
 
         game.roll(3);
 
         Assert.assertEquals(3, game.thePlaceOfTheOnlyPlayer());
     }
 
+    private static class SinglePlayerGameBuilder {
+        private boolean inPenaltyBox;
+        private int startingPlace;
+
+        public SinglePlayerGameBuilder withPlayerInPenaltyBox(boolean inPenaltyBox) {
+            this.inPenaltyBox = inPenaltyBox;
+            return this;
+        }
+
+        public SinglePlayerGameBuilder withPlayerStartingAtPlace(int place) {
+            this.startingPlace = place;
+            return this;
+        }
+
+        public SinglePlayerGame build() {
+            return new SinglePlayerGame(startingPlace, inPenaltyBox);
+        }
+    }
     private static class SinglePlayerGame extends Game {
         public SinglePlayerGame(int theStartingPlaceOfTheOnlyPlayer, boolean isTheOnlyPlayerInThePenaltyBox) {
             super(message -> {});
             add("::the only player's name::");
             places[0] = theStartingPlaceOfTheOnlyPlayer;
             inPenaltyBox[0] = isTheOnlyPlayerInThePenaltyBox;
-        }
-
-        // REFACTOR Test Data Builder pattern: http://defragdev.com/blog/?p=147
-        public static SinglePlayerGame withPlayerNotInPenaltyBoxStartingAtPlace(final int theStartingPlaceOfTheOnlyPlayer) {
-            return new SinglePlayerGame(
-                    theStartingPlaceOfTheOnlyPlayer,
-                    false );
         }
 
         public int thePlaceOfTheOnlyPlayer() {
