3c91a535f8027491783e7d20aff352b1ab30e097
J. B. Rainsberger <me@jbrains.ca>
2018-12-11 14:20:26 +0200

    Documented behavior in Board that prevents simplifying the design.

--- src/main/java/com/adaptionsoft/games/trivia/Board.java
+++ src/main/java/com/adaptionsoft/games/trivia/Board.java
@@ -4,6 +4,9 @@ public class Board {
 
     public static final String[] repeatingCategoryNames = new String[]{"Pop", "Science", "Sports", "Rock"};
 
+    // SMELL The else case here looks like a historical accident and not intentional behavior.
+    // Is it safe to change the behavior when place is "off the board"? If we could do that,
+    // then we could remove the ugly special case here.
     public static String category(int place) {
         if (place >= 0 && place < 12)
             return repeatingCategoryNames[place % repeatingCategoryNames.length];
