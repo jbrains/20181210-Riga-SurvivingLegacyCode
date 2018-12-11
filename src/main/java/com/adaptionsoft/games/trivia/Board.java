d80ce9ac214a0f9a3cd49c554a020e5b627b117e
J. B. Rainsberger <me@jbrains.ca>
2018-12-11 14:19:22 +0200

    Removed duplication.

--- src/main/java/com/adaptionsoft/games/trivia/Board.java
+++ src/main/java/com/adaptionsoft/games/trivia/Board.java
@@ -1,11 +1,12 @@
 package com.adaptionsoft.games.trivia;
 
 public class Board {
-    public static String category(int place) {
-        final String[] rotatingCategoryNames = {"Pop", "Science", "Sports", "Rock"};
 
+    public static final String[] repeatingCategoryNames = new String[]{"Pop", "Science", "Sports", "Rock"};
+
+    public static String category(int place) {
         if (place >= 0 && place < 12)
-            return rotatingCategoryNames[place % 4];
+            return repeatingCategoryNames[place % repeatingCategoryNames.length];
         else
             return "Rock";
     }
