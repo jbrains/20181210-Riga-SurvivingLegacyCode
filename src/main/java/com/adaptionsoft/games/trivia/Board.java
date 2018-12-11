5f6d36ef27193dafe36e60202ad4890c402d9e25
J. B. Rainsberger <me@jbrains.ca>
2018-12-11 14:18:17 +0200

    Removed duplication.

--- src/main/java/com/adaptionsoft/games/trivia/Board.java
+++ src/main/java/com/adaptionsoft/games/trivia/Board.java
@@ -4,19 +4,9 @@ public class Board {
     public static String category(int place) {
         final String[] rotatingCategoryNames = {"Pop", "Science", "Sports", "Rock"};
 
-        if (place == 0) return rotatingCategoryNames[place % 4];
-        if (place == 4) return rotatingCategoryNames[place % 4];
-        if (place == 8) return rotatingCategoryNames[place % 4];
-        if (place == 1) return rotatingCategoryNames[place % 4];
-        if (place == 5) return rotatingCategoryNames[place % 4];
-        if (place == 9) return rotatingCategoryNames[place % 4];
-        if (place == 2) return rotatingCategoryNames[place % 4];
-        if (place == 6) return rotatingCategoryNames[place % 4];
-        if (place == 10) return rotatingCategoryNames[place % 4];
-        if (place == 3) return rotatingCategoryNames[place % 4];
-        if (place == 7) return rotatingCategoryNames[place % 4];
-        if (place == 11) return rotatingCategoryNames[place % 4];
-
-        return "Rock";
+        if (place >= 0 && place < 12)
+            return rotatingCategoryNames[place % 4];
+        else
+            return "Rock";
     }
 }
