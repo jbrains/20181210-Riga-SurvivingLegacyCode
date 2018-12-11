aa5ad0271717d63d37ed58aa88ed25611f6e5741
J. B. Rainsberger <me@jbrains.ca>
2018-12-11 16:47:06 +0200

    We now know how to iterate through questions by category name, as long as we don't run out of questions.

--- src/test/java/ca/jbrains/trivia/test/IterateOverQuestionsByCategoryTest.java
+++ src/test/java/ca/jbrains/trivia/test/IterateOverQuestionsByCategoryTest.java
@@ -1,7 +1,6 @@
 package ca.jbrains.trivia.test;
 
-import io.vavr.collection.List;
-import io.vavr.collection.Traversable;
+import io.vavr.collection.*;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -17,6 +16,60 @@ public class IterateOverQuestionsByCategoryTest {
                         List.of("::question 1::", "::question 2::", "::question 3::")
                 ).take(9)
         );
+    }
+
+    @Test
+    public void cycleThroughQuestionsInMultipleCategories() throws Exception {
+        Assert.assertEquals(
+                List.of(
+                        "::rock question 1::",
+                        "::rock question 2::",
+                        "::science question 1::",
+                        "::pop question 1::",
+                        "::rock question 3::",
+                        "::sports question 1::",
+                        "::science question 2::",
+                        "::sports question 2::",
+                        "::sports question 3::",
+                        "::sports question 4::",
+                        "::science question 3::",
+                        "::rock question 4::",
+                        "::pop question 2::"
+                ),
+                iterateOverQuestionsByCategory(
+                        HashMap.of(
+                                "rock", generateQuestions("rock", 5).iterator(),
+                                "science", generateQuestions("science", 5).iterator(),
+                                "pop", generateQuestions("pop", 5).iterator(),
+                                "sports", generateQuestions("sports", 5).iterator()
+                        ),
+                        Stream.of(
+                                "rock",
+                                "rock",
+                                "science",
+                                "pop",
+                                "rock",
+                                "sports",
+                                "science",
+                                "sports",
+                                "sports",
+                                "sports",
+                                "science",
+                                "rock",
+                                "pop"))
+        );
+    }
+
+    private Traversable<String> iterateOverQuestionsByCategory(HashMap<String, Iterator<String>> questionsByCategoryName, Stream<String> categoryNames) {
+        return categoryNames.map(categoryName -> getNextQuestion(questionsByCategoryName, categoryName));
+    }
+
+    private String getNextQuestion(HashMap<String, Iterator<String>> questionsByCategoryName, String categoryName) {
+        return questionsByCategoryName.get(categoryName).get().next();
+    }
+
+    private Stream<String> generateQuestions(String categoryName, int howMany) {
+        return Stream.rangeClosed(1, howMany).map(n -> String.format("::%s question %d::", categoryName, n));
     }
 
     private Traversable<String> iterateOverQuestions(List<String> questions) {
