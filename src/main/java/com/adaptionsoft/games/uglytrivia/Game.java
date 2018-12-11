d640a5cc01ef903e594090b009bd00b7d258c6e9
J. B. Rainsberger <me@jbrains.ca>
2018-12-11 14:13:14 +0200

    Replaced legacy code with pure function.

--- src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -105,16 +105,7 @@ public class Game {
 	}
 
 	private String currentCategory() {
-		if (places[currentPlayer] == 0) return "Pop";
-		if (places[currentPlayer] == 4) return "Pop";
-		if (places[currentPlayer] == 8) return "Pop";
-		if (places[currentPlayer] == 1) return "Science";
-		if (places[currentPlayer] == 5) return "Science";
-		if (places[currentPlayer] == 9) return "Science";
-		if (places[currentPlayer] == 2) return "Sports";
-		if (places[currentPlayer] == 6) return "Sports";
-		if (places[currentPlayer] == 10) return "Sports";
-		return "Rock";
+		return category(places[currentPlayer]);
 	}
 	
 	public static String category(int place) {
