4ddbd95cf45e94db6067062327926eceda51c41b
J. B. Rainsberger <me@jbrains.ca>
2018-12-11 14:11:17 +0200

    We confirm how the Game chooses the Category for each Place on the Board.

--- src/test/java/ca/jbrains/trivia/test/ChooseCategoryByPlaceTest.java
+++ src/test/java/ca/jbrains/trivia/test/ChooseCategoryByPlaceTest.java
@@ -1,6 +1,8 @@
 package ca.jbrains.trivia.test;
 
 import com.adaptionsoft.games.uglytrivia.Game;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,5 +14,13 @@ public class ChooseCategoryByPlaceTest {
         for (int place = 0; place < 12; place++) {
             Assert.assertEquals(expectedCategoriesByPlaceOnTheBoard[place], Game.category(place));
         }
+    }
+
+    // QUESTION Is this behavior important, or an accident? It would be more convenient
+    // to make this behavior undefined.
+    @Property
+    public boolean everyOtherPlaceIsRock(@ForAll int place) {
+        if (place >= 0 && place < 12) return true;
+        return "Rock".equalsIgnoreCase(Game.category(place));
     }
 }
